fb847c33e4634926c9a556badd27656da9bdff02
Theofanis Bekiaris <43439535+THEOFANIS-BEKIARIS@users.noreply.github.com>
2018-12-28 21:12:27 +0200

    Add files via upload

--- calfeatures.m
+++ calfeatures.m
@@ -13,7 +13,8 @@ for i = 1:1
     feature5 = zeros(size(savedData(i).spikeEst,2),1);
     feature6 = zeros(size(savedData(i).spikeEst,2),1);
     feature7 = zeros(size(savedData(i).spikeEst,2),1);
-    
+    feature8 = zeros(size(savedData(i).spikeEst,2),1);
+    feature9 = feature1;
     firstZeroVect = zeros(size(savedData(i).spikeEst,2),1);
     secondZeroVect = zeros(size(savedData(i).spikeEst,2),1);
     thirdZeroVect = zeros(size(savedData(i).spikeEst,2),1);
@@ -42,6 +43,11 @@ for i = 1:1
         while( sign(deriv(thirdZero)) == -signOfPeakDer  && thirdZero < 4*lengthSpike+1)
         thirdZero = thirdZero + 1 ;
         end
+        %Deytero Akrotato
+        feature8(r) = thirdZero;
+        %Max timh tou spike
+        if(feature9(r) < 0), feature9(r) = thirdZero; else feature9(r) = firstPeakPosition; end
+        %Trito Mhdeniko
         while( sign(deriv(thirdZero)) == signOfPeakDer  && thirdZero < 4*lengthSpike)
         thirdZero = thirdZero + 1 ;
         end
@@ -53,23 +59,24 @@ for i = 1:1
         feature5(r) = trapz(secondZero:thirdZero,savedData(i).spikeEst(secondZero:thirdZero,r));
         feature6(r) = savedData(i).spikeEst(round((firstZero+thirdZero)/2),r);
         feature7(r) = savedData(i).spikeEst(round((secondZero+thirdZero)/2),r);
+        
         firstZeroVect(r) = firstZero;
         secondZeroVect(r) = secondZero;
         thirdZeroVect(r) = thirdZero;
         
     end
     
-    savedData(i).features = [feature1 feature2 feature3 feature4 feature5 feature6 feature7]; 
+    savedData(i).features = [feature1 feature2 feature3 feature4 feature5 feature6 feature7 feature8 feature9]; 
 end
 
 %{P
 figure(1)
 color = savedData(i).classEst;
-gscatter(savedData(i).features(:,1), savedData(i).features(:,7),color,'kgrb','o*',8,'on');
+gscatter(savedData(i).features(:,1), savedData(i).features(:,9),color,'kgrb','o*',8,'on');
 legend({'Noise', 'Class1','Class2', 'Class3'});
 %}
 
-  %{Plot twn spikes pou vrikame apo ton kanona
+  %{Plot twn spikes feature7
 figure(2)
 for g = 1:130
 plot( savedData(i).spikeEst(:,g))
@@ -77,12 +84,42 @@ hold on
 plot(firstZeroVect(g),savedData(i).spikeEst(firstZeroVect(g),g),'b*')
 plot(secondZeroVect(g),savedData(i).spikeEst(secondZeroVect(g),g),'g*')
 plot(thirdZeroVect(g),savedData(i).spikeEst(thirdZeroVect(g),g),'y*')
-plot(round((thirdZeroVect(g)+secondZeroVect(g))/2),savedData(i).spikeEst(round((thirdZeroVect(g)+secondZeroVect(g))/2),g),'m*')
+plot(round((thirdZeroVect(g)+secondZeroVect(g))/2),feature7(g),'m*')
 plot(2*lengthSpike+1,savedData(i).features(g,1),'r*')
 name = ['Spike' num2str(g)];
 title(name);
 %pause(0.5);
 %hold off
 end
-legend('Kumatomorfes','firstZero','secondZero','ThirdZero','Endiamesa se 2,3 Zeros')
+legend('Kumatomorfes','firstZero','secondZero','ThirdZero','Endiamesa se 2,3 Zeros','PrwtoAkrotato')
+%}
+
+
+ %{Plot twn spikes me feature8
+figure(3)
+for g = 1:130
+plot( savedData(i).spikeEst(:,g))
+hold on 
+plot(2*lengthSpike+1,savedData(i).features(g,1),'r*')
+plot(feature8(g),savedData(i).spikeEst(feature8(g),g),'m*')
+name = ['Spike' num2str(g)];
+title(name);
+%pause(0.5);
+%hold off
+end
+legend('Kumatomorfes','PrwtoAkrotato','DeuteroAkrotato')
+%}
+
+%{Plot twn spikes me feature9
+figure(4)
+for g = 1:130
+plot( savedData(i).spikeEst(:,g))
+hold on 
+plot(feature9(g),savedData(i).spikeEst(feature9(g),g),'m*')
+name = ['Spike' num2str(g)];
+title(name);
+%pause(0.5);
+%hold off
+end
+legend('Kumatomorfes','MaxValues')
 %}
