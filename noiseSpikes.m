841f6a71b966dcaffb0f112068e3ca90a74a626c
Theofanis Bekiaris <43439535+THEOFANIS-BEKIARIS@users.noreply.github.com>
2018-12-29 21:39:15 +0200

    Add files via upload

--- noiseSpikes.m
+++ noiseSpikes.m
@@ -1,35 +1,62 @@
 close all
 numOfnoisePoints = zeros(4,1);
-numOfSpikes = zeros(4,1);
+numOfrealSpikes = zeros(4,1);
+numOfGivenSpikes = zeros(4,1);
 %% Ypologismos twn realSpikes kai noiseSpikes
 %-----Erwthma 2.3-----
 for i = 1:4
-    name = ['Data/Data_Eval_E_' num2str(i)];
+    name = ['Data_Eval_E_' num2str(i)];
 	load(name)
-    numOfSpikes(i) = length(spikeTimes);    
-    maxRange = 26;
+    numOfGivenSpikes(i) = length(spikeTimes);    
+    maxRange = 40;
+    sm = 0;
     noisePointsIndex = [];
     savedData(i).classEst = zeros(size(savedData(i).spikeTimesEst,2),1);
     for r = 1:size(savedData(i).spikeFirstPeakTimes,2)
         minDictance = savedData(i).spikeFirstPeakTimes(r) - spikeTimes;
         minDictance(minDictance < 0) = maxRange + 1000; %aporiptoume arnhtikes times
         [minDictance ,index] = min(minDictance);
-        if minDictance > maxRange 
+        if minDictance > maxRange || minDictance < sm
             noisePointsIndex = [noisePointsIndex r];
         else
             savedData(i).classEst(r) = spikeClass(index);
+            
+            %{ 
+%Pragmatika Spikes
+            figure(3)
+            g = index;
+            e = 83;
+            m = savedData(i).spikeFirstPeakTimes(r);
+            subplot(1,2,2)
+            plot((data(m-e:m+e)))
+            hold on 
+            plot(e+1,data(m),'r*')
+            hold off
+            subplot(1,2,1)
+            m = spikeTimes(index);
+            subplot(1,2,1)
+            plot((data(m-e:m+e+30)))
+            hold on 
+            plot(e+1,data(m),'r*')
+            hold off
+            pause(1.5)
+            name = ['Spike' num2str(g)];
+            title(name);
+            pause(0.0001);
+            %hold off
+            %}
         end
     end
     noisePointTimes = savedData(i).spikeFirstPeakTimes(noisePointsIndex);
     RealSpikesTimes = savedData(i).spikeFirstPeakTimes;
     RealSpikesTimes(noisePointsIndex) = [];
     numOfnoisePoints(i) = length(noisePointTimes);
-  
-    %{    
-    %Noise Spikes
+    numOfrealSpikes(i) = length(RealSpikesTimes);
+    %{
+ %Noise Spikes
     figure(1)
     for g = 1:length(noisePointTimes)
-    plot( (data(noisePointTimes(g)-32:noisePointTimes(g)+32)))
+    plot( (data(noisePointTimes(g)-32:noisePointTimes(g)+70)))
     hold on 
     plot(33,data(noisePointTimes(g)),'r*')
     name = ['Spike' num2str(g)];
@@ -39,8 +66,8 @@ for i = 1:4
     end
     %}
     
-    %{
-    %Pragmatika Spikes
+    %{   
+ %Pragmatika Spikes
     figure(2)
     for g = 1:length(RealSpikesTimes)
     plot( (data(RealSpikesTimes(g)-32:RealSpikesTimes(g)+32)))
@@ -52,11 +79,17 @@ for i = 1:4
     %hold off
     end
     %}
+    
+    
 end
 
 %Arxikh Diafora apotelesmatwn
 ArxikhDiafora
 %Meta thn eka8arhsh twn noise point
-TelikhDiafora = ArxikhDiafora + numOfnoisePoints
-%Pososto epituxias
-success = 100 - (TelikhDiafora./numOfSpikes).*100
+TelikhDiafora = abs(numOfGivenSpikes - numOfrealSpikes)
+
+%Pososto epituxia arxika
+successBegin = 100 - (ArxikhDiafora./numOfGivenSpikes).*100
+
+%Pososto epituxias telika
+successEnd = 100 - (TelikhDiafora./numOfGivenSpikes).*100
